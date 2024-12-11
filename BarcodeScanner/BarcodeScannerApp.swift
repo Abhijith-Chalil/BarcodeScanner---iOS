c63e4f8cbc8e60e109b047d2d356a801c01c138e
Abhijith C <abhijithchalilabc@gmail.com>
2024-12-11 23:20:58 +0530

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Abhijith Chalil on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
